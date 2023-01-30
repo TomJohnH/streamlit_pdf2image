107438c53f6ca15bf52dc1de5a59c55d1b2cb83c
Thomas.Ellyatt <thomas.ellyatt@gmail.com>
2023-01-30 21:32:20 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pdf2image
+import zipfile
+import os
+
+pdf_uploaded = st.file_uploader("Select a file", type="pdf")
+button = st.button("Confirm")
+image_down = []
+if button and pdf_uploaded is not None:
+
+    if pdf_uploaded.type == "application/pdf":
+        images = pdf2image.convert_from_bytes(pdf_uploaded.read(), poppler_path="poppler/library/bin")
+        for i, page in enumerate(images):
+            st.image(page, use_column_width=True)
+            st.download_button("Download", data=page, file_name=f"Image_{i}.png")
+            image_down.append(page)
